04a8b9b585fee70365f57cae3f58c0f3282bd81a
Samuel Brigha <sam.brigham@yale.edu>
2020-05-14 16:57:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_richie][:languages] => ["C", "Assembly"]
+programmer_hash[:dennis_richie][:languages] => "C", "Assembly"]
 return programmer_hash
 end
